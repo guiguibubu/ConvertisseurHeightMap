4c4c663b1eab6c6c8d2c019c64f7bcc56252de6b
guiguibubu <guibu44@hotmail.fr>
2018-09-20 21:06:43 -0400

    Lecture fichier OK

--- ConvertisseurHeightMap/ConvertisseurHeightMap.cpp
+++ ConvertisseurHeightMap/ConvertisseurHeightMap.cpp
@@ -1,13 +1,60 @@
-
 #include "ConvertisseurHeightMap.h"
+#include <string>
+#include <fstream>
+#include <iostream>
 
-int main() {
+using namespace std;
 
-   LireFichierHeightMap();
-   ConstruireTerrain(1.0f, 1.0f);
-   CalculerNormales();
-   ConstruireIndex();
-   EnregistrerTout();
+void ConvertisseurHeightMap::lireFichierHeightMap() {
 
-   return 0;
+   ifstream in{ heightMapPath };
+   
+   int entierLu; // entier lu dans le fichier
+   int indexLigne = 0;
+   int indexColonne = 0;
+   
+   int i = 0;
+   while (in >> entierLu) {
+      // 1ere ligne correspond aux nombres de lignes
+      if (i == 0) {
+         dy = entierLu;
+      }
+      // 2eme ligne correspond aux nombres de colonnes
+      else if (i == 1) {
+         dx = entierLu;
+      }
+      // On lit la hauteurs pour chaque sommet suivant l'axe des "x" croissant puis l'axe des "y" croissants
+      else {
+         indexColonne = (i - 2) % dx;
+         indexLigne = (i - 2) / dx;
+
+         sommets.push_back(CSommetTerrain(indexColonne, indexLigne, entierLu));
+      }
+      i++;
+   }
+}
+
+void ConvertisseurHeightMap::afficherSommetsXYZ(ostream& stream) {
+   for (CSommetTerrain sommetTerrain : sommets) {
+      stream << "x = " << sommetTerrain.getX()
+         << "; y = " << sommetTerrain.getY()
+         << "; z = " << sommetTerrain.getZ()
+         << endl;
+   }
+}
+
+void ConvertisseurHeightMap::afficherHeightMapPath(std::ostream& stream) {
+  stream << "HeightMapPath : " << heightMapPath  << endl;
+}
+void ConvertisseurHeightMap::afficherNbLigne(std::ostream& stream) {
+   stream << "Nb Ligne : " << dy << endl;
+}
+void ConvertisseurHeightMap::afficherNbColonne(std::ostream& stream) {
+   stream << "Nb Colonne : " << dx << endl;
+}
+void ConvertisseurHeightMap::afficherNbSommets(std::ostream& stream) {
+   stream << "Nb Sommet : " << nbSommets << endl;
+}
+void ConvertisseurHeightMap::afficherNbPolygone(std::ostream& stream) {
+   stream << "Nb Polygone : " << nbPolygones << endl;
 }
